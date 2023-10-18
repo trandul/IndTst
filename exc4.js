b4edcadc0056b3f2c6031be18fced4e509548ebb
trandul <trandul@yandex.ru>
2023-10-19 00:25:02 +0300

    Update exc4.js

--- exc4.js
+++ exc4.js
@@ -22,6 +22,7 @@ console.log(arr[2]);
     </body>
 </html>
 //3. Напишите код JavaScript который выводит алертом содержимое поля ввода, возведенное в квадрат
+// нужно ли сообщение в случае, если введено нечисловое значение?
 <!DOCTYPE html>
 <html>
     <head>
@@ -33,6 +34,81 @@ console.log(arr[2]);
 		function processNumber(){
 			let inpStr = document.getElementById('number').value;
 			alert(isFinite(inpStr)?Math.pow(parseInt(inpStr),2):'Введите число');
+        </script>
+    </body>
+</html>
+//4. Написать функцию для кнопки (id=”buttonAdd”) которая добавляет новую строчку к таблице (id=”memberTable”) с пустыми полями имя и фамилия (тип полей input), но заполненным полем № п\п (тоже тип input ,но недоступен для редактирования).
+//Начальные строки создать программно или как html или нет разницы?
+<!DOCTYPE html>
+<html>
+    <head>
+    </head>
+    <body>
+	<table id = "table">
+            <tr>
+                <th>№ п\п</th>
+                <th>Имя</th>
+                <th>Фамилия</th>
+            </tr>
+            <tr>
+                <td>1</td>
+                <td>Иван</td>
+                <td>Пётр</td>
+            </tr>
+            <tr>
+                <td>2</td>
+                <td>Иванов</td>
+                <td>Петров</td>
+            </tr>
+        </table>
+	<button id="addButton" onclick='addRow()'>Добавить строку</button>
+        <script type="text/javascript">
+		var idx = 3;
+		function addRow(){
+			let table = document.getElementById('table');
+			table.insertAdjacentHTML('beforeend', '<tr><td><input readonly value = "'+idx+'" size = 4/></td> <td><input placeholder = "Имя"></input></td> <td><input placeholder = "Фамилия"></input></td> </tr>')
+			idx++;
+		}
+        </script>
+    </body>
+</html>
+//5. Написать функцию для кнопки валидации данных (id=”validButton”), которая проверяет наличие пустых полней из таблицы ( задания 4 ) и если поля заполнены, то проверяет кол-во введенных символов ( >= 2). 
+//При успешной валидации выводит на экран алерт: «Успешно». Иначе «Не выполнены все условия заполнения таблицы».
+<!DOCTYPE html>
+<html>
+    <head>
+    </head>
+    <body>
+	<table id = "table">
+            <tr>
+                <th>№ п\п</th>
+                <th>Имя</th>
+                <th>Фамилия</th>
+            </tr>
+            <tr>
+                <td>1</td>
+                <td>Иван</td>
+                <td>Пётр</td>
+            </tr>
+            <tr>
+                <td>2</td>
+                <td>Иванов</td>
+                <td>Петров</td>
+            </tr>
+        </table>
+	<button id="addButton" onclick='addRow()'>Добавить строку</button>
+	<button id="validateButton" onclick='validate()'>Валидация</button>
+        <script type="text/javascript">
+		var idx = 3;
+		function addRow(){
+			let table = document.getElementById('table');
+			table.insertAdjacentHTML('beforeend', '<tr><td><input readonly value = "'+idx+'" size = 4 class = "idx"/></td> <td><input placeholder = "Имя"></input></td> <td><input placeholder = "Фамилия"></input></td> </tr>')
+			idx++;
+		}
+		function validate(){
+			let inputs = Array.prototype.slice.call(document.getElementsByTagName('input'),0);
+			inputs.length>0?alert(inputs.filter(x=>(x.className !='idx' && x.value.length<2)).length>0?"Не выполнены все условия заполнения таблицы":"Успешно"):null;
+		}
         </script>
     </body>
 </html>
